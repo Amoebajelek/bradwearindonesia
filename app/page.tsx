fb417b429b858dc7de1a7f787012dae77ca62393
amoebajelek <amoebajelek@gmail.com>
2025-08-28 19:07:27 +0700

    Change background color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full min-h-screen bg-slate-800">
+    <div className="flex flex-col w-full min-h-screen bg-white">
       <Navbar />
     </div>
   );
