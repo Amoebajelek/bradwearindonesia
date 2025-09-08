6620a6675ad0876b026f2bd37db56b6f5c649c65
amoebajelek <amoebajelek@gmail.com>
2025-09-09 03:36:13 +0700

    Content-Add products

--- app/_components/Products.tsx
+++ app/_components/Products.tsx
@@ -6,63 +6,99 @@ const products = [
   {
     id: 1,
     name: "Series Ventura",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+    sells: "2RB+ terjual",
+    desc: "A card component has a figure, a body part, and inside body there are title and actions parts.",
     image: "/Carousel-1.jpg",
   },
   {
     id: 2,
-    name: "Series 3",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+    name: "Series Yuroi",
+    sells: "2RB+ terjual",
+    desc: "A card component has a figure, a body part, and inside body there are title and actions parts.",
     image: "/Carousel-2.jpg",
   },
   {
     id: 3,
-    name: "Series 2",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+    name: "Series 3",
+    sells: "2RB+ terjual",
+    desc: "A card component has a figure, a body part, and inside body there are title and actions parts.",
     image: "/Carousel-3.jpg",
   },
   {
     id: 4,
-    name: "Series 2",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+    name: "Series PDH",
+    sells: "2RB+ terjual",
+    desc: "A card component has a figure, a body part, and inside body there are title and actions parts.",
     image: "/Carousel-3.jpg",
   },
   {
     id: 5,
-    name: "Series 2",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+    name: "Series Robotic",
+    sells: "2RB+ terjual",
+    desc: "A card component has a figure, a body part, and inside body there are title and actions parts.",
     image: "/Carousel-3.jpg",
   },
-  {
-    id: 6,
-    name: "Series 2",
-    desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
-    image: "/Carousel-3.jpg",
-  },
+  // {
+  //   id: 6,
+  //   name: "Series 1",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
+  // {
+  //   id: 7,
+  //   name: "Series Strazard",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Series Strazard",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
+  // {
+  //   id: 9,
+  //   name: "Series 2",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Series Brad V-2",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Series Brad V-1",
+  //   desc: "A card component has a figure, a body part, and inside body there are title and actions parts|",
+  //   image: "/Carousel-3.jpg",
+  // },
 ];
 
 export default function Products() {
   return (
     <>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3">
         {products.map((product) => (
           <div className="card bg-white shadow-sm" key={product.id}>
             <figure>
               <Image
                 src={product.image}
                 alt="Product 1"
-                className=" object-full"
+                className="w-full"
                 width={360}
-                height={128}
+                height={24}
                 loading="lazy"
               />
             </figure>
-            <div className="card-body">
-              <h2 className="card-title uppercase text-2xl font-bold overflow-hidden min-w-full">BRANDWEAR <span className=" text-green-500">{product.name}</span></h2>
-              <p className="">{product.desc}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
+            <div className="card-body p-4 pt-0 gap-0">
+              <p className="card-title uppercase text-2xl font-bold text-green-500">
+                {product.name}
+              </p>
+              <p className="text-slate-500 text-xs font-medium">
+                {product.sells}
+              </p>
             </div>
           </div>
         ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import Navbar from "./_components/Navbar";
 import Products from "./_components/Products";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full min-h-screen bg-slate-50">
       <Navbar />
-      <div className="flex flex-col w-full px-20 justify-center">
-        <div className="flex flex-col">
-          <p className="uppercase text-base font-regular text-gray-500">
+      <div className="flex flex-col w-full p-20 justify-center gap-5">
+        <div className="flex flex-col gap-0">
+          <p className="uppercase text-sm font-regular text-gray-500">
             best sellers
           </p>
           <h2 className="uppercase text-4xl font-bold">comfort every day</h2>
         </div>
         <Products />
+        <div className="card-actions justify-center">
+          <Link href="#">
+            <button className="btn border-none bg-green-500 text-white">
+              View more
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
